bf77e06dbf02d458850a0c1e3308383a6fb0bc82
kwagyei <kwabenasasuboakye@gmail.com>
2024-01-21 23:59:45 +0000

    Connecting BE to FE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [listOfPosts,setListOfPosts] = useState([])
+
+  useEffect( () => {
+
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setListOfPosts(response.data)
+    })
+
+  }, [] )
+
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { listOfPosts.map( (item,key) => (
+
+      <div className='container mt-3'>
+        <div className='post bg-light p-3 rounded'>
+          <div className='title mb-2 fw-bold text-start'>{item.title}</div>
+          <div className='body'>{item.postText}</div>
+          <div className='footer mt-2 text-muted text-end'>{item.userName}</div>
+     </div>
+</div>
+      
+      
+      
+      ))}
+
+
+
     </div>
   );
 }
